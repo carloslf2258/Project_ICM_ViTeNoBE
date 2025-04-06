28e4004f41ab4659fec00b44a1a07d5de88c45e4
carloslf2258 <carlos.verenzuela@ua.pt>
2025-04-06 23:55:49 +0100

    "Aceito" button works

--- app/src/main/java/com/example/vitenobe/CreateAccountActivity.kt
+++ app/src/main/java/com/example/vitenobe/CreateAccountActivity.kt
@@ -1,6 +1,7 @@
+package com.example.vitenobe
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.vitenobe.R
 
 class CreateAccountActivity : AppCompatActivity() {
 
